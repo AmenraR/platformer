895bff057d69ad3266bcbf4a1676a680f5474287
Ezra Alarid <eja.117@gmail.com>
2014-11-18 15:00:42 -0800

    Initial update loaded map on platformer

---
+++ core/src/com/alaride/platformer/view/GameScreen.java
@@ -0,0 +1,59 @@
+package com.alaride.platformer.view;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class GameScreen implements Screen{
+    public TiledMap map;
+    public OrthogonalTiledMapRenderer renderer;
+    public OrthographicCamera camera;
+
+    public GameScreen() {
+        //loads level map from my assets folder
+        map = new TmxMapLoader().load("map/level_01.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map, 1/70f);
+        camera= new OrthographicCamera(14f, 14f);
+    }
+
+    @Override
+    public void render(float delta) {
+        //updates all, every moment the platformer is active
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
