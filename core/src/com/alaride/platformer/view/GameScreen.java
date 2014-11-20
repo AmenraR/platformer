1639e55fc97e550cfbe2b02e654161f154f81335
Ezra Alarid <eja.117@gmail.com>
2014-11-20 14:55:22 -0800

    moises lesson 2 receives the window dimensions and applies it to the camera

--- core/src/com/alaride/platformer/view/GameScreen.java
+++ core/src/com/alaride/platformer/view/GameScreen.java
@@ -1,5 +1,6 @@
 package com.alaride.platformer.view;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
@@ -16,7 +17,12 @@ public class GameScreen implements Screen{
         //loads level map from my assets folder
         map = new TmxMapLoader().load("map/level_01.tmx");      //takes path to map
         renderer = new OrthogonalTiledMapRenderer(map, 1/70f);      //defines the unit per pixel
-        camera= new OrthographicCamera(14f, 14f);       //defines the size of camera in units
+
+        float width = Gdx.graphics.getWidth();      //looks through the graphics library and pulls the window width and saves it into a variable
+        float height = Gdx.graphics.getHeight();        //grabs the window height from the graphics library and saves it into a variable
+
+        camera= new OrthographicCamera(14f, 14f*(height/width));       //defines the size of camera in units
+        camera.position.set(camera.viewportWidth/2, camera.viewportHeight/2, 0f);       //sets the camera position to the saved camera height and width
     }
 
     @Override
