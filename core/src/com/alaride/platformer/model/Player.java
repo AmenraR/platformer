f2fcf09d73020f8cf95ac9a06820cdde45216906
Ezra Alarid <eja.117@gmail.com>
2014-12-05 00:20:24 -0800

    moises lesson #5 accounts for the number of rows in spriteframes and store the number in spritesheet to select aliens from the sheet display player

--- core/src/com/alaride/platformer/model/Player.java
+++ core/src/com/alaride/platformer/model/Player.java
@@ -2,14 +2,51 @@ package com.alaride.platformer.model;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+
+import javax.xml.soap.Text;
 
 public class Player {
     public Vector2 position;
     public Texture spritesheet;
+    public TextureRegion[] spriteFrames;
 
     public Player() {
         position = new Vector2(0,0);    //creates an origin for the player's position
-        spritesheet = new Texture(Gdx.files.internal("img/aliens(1).png"));       //looks into the assets folder and searches for aliens and saves it in the spritesheet variable
+        spritesheet = new Texture(Gdx.files.internal("img/aliens 1).png"));       //looks into the assets folder and searches for aliens and saves it in the spritesheet variable
+
+        TextureRegion[][] spriteSheetFrames = TextureRegion.split(spritesheet, 70, 100);
+
+        int counter = 0;
+
+        for(int row = 0; row < spriteSheetFrames.length; row++){
+            for(int column = 0; column < spriteSheetFrames[row].length; column++){
+                counter++;
+            }
+        }
+
+        spriteFrames = new TextureRegion[counter];  //saves the amount of rows in spriteFrames TO store
+
+        counter = 0;
+
+        for (TextureRegion[] row : spriteSheetFrames){      //looks at spriteSheetFrames and stores it in row
+            for (TextureRegion sprite : row){       //checks every row, and takes out every column, then stores it in sprite
+                spriteFrames[counter++] = sprite;       //puts the aliens from sprite into spriteFrames and counts them.
+
+            }
+
+        }
+    }
+
+    public void draw(Batch spriteBatch){
+        spriteBatch.draw(spriteFrames[37], 0 , 0 , 70, 100);
+
+    }
+
+    public void update(float deltaTime){
+
+
     }
 }
--- core/src/com/alaride/platformer/view/GameScreen.java
+++ core/src/com/alaride/platformer/view/GameScreen.java
@@ -1,9 +1,11 @@
 package com.alaride.platformer.view;
 
+import com.alaride.platformer.model.Player;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
@@ -13,6 +15,9 @@ public class GameScreen implements Screen{
     public TiledMap map;
     public OrthogonalTiledMapRenderer renderer;
     public OrthographicCamera camera;
+
+    public SpriteBatch spriteBatch;
+    public Player player;
 
     public GameScreen() {
         //loads level map from my assets folder
@@ -24,6 +29,9 @@ public class GameScreen implements Screen{
 
         camera= new OrthographicCamera(14f, 14f*(height/width));       //defines the size of camera in units
         camera.position.set(camera.viewportWidth/2, camera.viewportHeight/2, 0f);       //sets the camera position to the saved camera height and width
+
+        spriteBatch = new SpriteBatch();        //grants the ability to draw the textures on the screen in one print
+        player = new Player();
     }
 
     @Override
@@ -37,6 +45,10 @@ public class GameScreen implements Screen{
         camera.update();        //updates position of camera
         renderer.setView(camera);       //renders map view
         renderer.render();      //renders itself
+
+        spriteBatch.begin();
+        player.draw(spriteBatch);             //readies spritebach and ends spritebatch
+        spriteBatch.end();
 
     }
 
